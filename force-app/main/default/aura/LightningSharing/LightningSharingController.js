487c94415b05305e883edefb29199ebf48a1bf1c
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-11-13 17:41:30 -0600

    label for what we're sharing

--- force-app/main/default/aura/LightningSharing/LightningSharingController.js
+++ force-app/main/default/aura/LightningSharing/LightningSharingController.js
@@ -1,6 +1,28 @@
 ({
 	doInit : function(component, undefined, helper) {
 		helper.reload(component);
+
+		var action = component.get("c.sayMyName");
+		action.setParams({
+			recordId: component.get("v.recordId")
+		});
+		action.setCallback(this, function (a) {
+			var state = a.getState();
+			if (state === "SUCCESS") {
+				console.log(a);
+				var rs = JSON.parse(a.getReturnValue());
+				component.set("v.recordName", rs.recordName);
+				component.set("v.sObjectName", rs.objectType);
+
+			} else if (state === "ERROR") {
+				let appEvent = $A.get("e.c:handleCallbackError");
+				appEvent.setParams({
+					"errors": a.getError()
+				});
+				appEvent.fire();
+			}
+		});
+		$A.enqueueAction(action);
 	},
 
 	stopProp : function(component, event) {
--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,10 @@
-sfdx force:org:create -a ltngshare -f config/project-scratch-def.json -s
+sfdx force:org:create -a ltngshare -f config/project-scratch-def.json -s -d 30
 sfdx force:source:push
 sfdx force:user:permset:assign -n TestingPerms
 
 # create some records
 sfdx force:data:tree:import -f data/PrivateTestObject__c.json
 sfdx force:data:tree:import -f data/ReadOnlyTestObject__c.json
+
+sfdx force:user:password:generate
+sfdx force:org:open
